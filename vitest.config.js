5e545937758a76fe676cae8a30ca770a999b9da8
st <sanjaythadani@gmail.com>
2023-11-07 14:55:49 -0600

    fix tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,12 @@
-import { fileURLToPath } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-    viteConfig,
+export default defineConfig(mode => mergeConfig(
+    viteConfig(mode),
     defineConfig({
         test: {
             environment: 'jsdom',
-            exclude: [...configDefaults.exclude, 'e2e/*'],
-            root: fileURLToPath(new URL('./', import.meta.url))
+            exclude: [...configDefaults.exclude, 'e2e/*']
         }
     })
-);
+));
